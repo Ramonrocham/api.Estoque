012a32d9b3d9f588c3eb5ddb42e97fc901f8ff60
ramonrocham <anacletorrocham@gmail.com>
2026-02-16 17:10:25 -0300

    feat: tratamento de erros para endpoint nao encontrado e erro interno

---
+++ src/routes/errorHandler.ts
@@ -0,0 +1,15 @@
+import type { ErrorRequestHandler, RequestHandler } from "express";
+
+export const notFoundRequest: RequestHandler = (req, res) => {
+    res.status(404).send({
+        status: 'error',
+        message: 'Endpoint não encontrado'
+    });
+}
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    res.status(500).send({
+        status: 'error',
+        message: 'Ocorreu um erro interno no servidor'
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import helmet from 'helmet';
 import router from './routes/index.js';
+import { errorHandler, notFoundRequest } from './routes/errorHandler.js';
 
 const server = express();
 
@@ -11,8 +12,16 @@ server.use(express.urlencoded({extended: true}));
 server.use('/api/v1', router);
 
 server.get('/', (req, res) => {
-    res.send({'message': 'Welcome to the Stock Management API!'});
+    res.send(
+        {
+            'message': 'Welcome to the Stock Management API!',
+            'seeMore': 'Github: https://github.com/ramonrocham/api.Estoque'
+        }
+);
 });
+
+server.use(notFoundRequest);
+server.use(errorHandler);
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
